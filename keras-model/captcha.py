696bcd69d8f9cd57969ab390a37dcf933151fcfa
Yidadaa <1143922395@qq.com>
2018-02-24 12:08:54 +0800

    refactor: 重构数据生成器，增加评估函数

--- keras-model/captcha.py
+++ keras-model/captcha.py
@@ -10,48 +10,94 @@ import numpy as np
 import json
 import os
 
-class Evaluate(Callback):
+
+def evaluate(y_true, y_pred):
+    """评估函数
+    Args:
+        y_true(List): 正确标签
+        y_pred(List): 预测结果
+    Return:
+        Acc(float): 准确率
+    """
+    y_true_t = np.argmax(y_true, axis=2).T
+    y_pred_t = np.argmax(y_pred, axis=2).T
+    acc = np.mean(map(np.array_equal, y_pred_t, y_true_t))
+    print("\033[0;31mCurrent acc: %f%%\033[0m" % (acc * 100))
+    return acc
+
+class Evaluator(Callback):
     """模型的评价函数类
 
     该类继承自keras.callbacks.Callback
     """
-    def __init__(self, model, gen, batch_num=10):
-        self.acc = []
-        self.gen = gen
-        self.model = model
-        self.batch_num = batch_num
+    def __init__(self, dataset):
+        """
+        Args:
+            dataset(Dataset): 数据集
+        """
+        self.accs = []
+        self.gen = dataset.gen(type='test')
 
     def on_epoch_end(self, epoch, logs=None):
-        acc = 0.0
-        data_gen = self.gen(128)
-        for i in range(self.batch_num):
-            X_test, y_test = next(data_gen)
-            y_pred = self.model.predict(X_test)
-
+        X_test, y_test = next(self.gen)
+        y_pred = self.model.predict(X_test)
+        acc = evaluate(y_test, y_pred)
         self.accs.append(acc)
-        print('\nCurrent Acc: %f\n'%acc)
 
-class Captcha:
+class Dataset():
+    """数据集读取类
+    """
+    def __init__(self, root='../data/', index_file='index.json', test_count=5000):
+        self.path_prefix = root
+        self.file_path = root + index_file
+        self.test_count = test_count
 
-    def __init__(self):
-        self.width, self.height, self.n_len, self.n_class = 240, 60, 5, 10
-        self.model = None
-        self.init_model()
+        self.train_index = []
+        self.train_label = []
+        self.test_index = []
+        self.test_label = []
+        self.load_data()
 
-    def gen(self, batch_size=96):
-        width, height = self.width, self.height
-        X = np.zeros((batch_size, height, width, 1), dtype=np.uint8)
-        y = [np.zeros((batch_size, self.n_class), dtype=np.uint8) for i in range(self.n_len)]
-        path_prefix = '../data/'
-        file_path = path_prefix + 'index.json'
-        with open(file_path) as f:
+    def load_data(self):
+        """读取索引数据到内存
+        """
+        with open(self.file_path) as f:
             src = json.loads(f.read())
             src = np.array(src)
         labels = src[:, 1]
         images = src[:, 0]
+        N = len(labels)
+        self.train_index = images[0:N - self.test_count]
+        self.train_label = labels[0:N - self.test_count]
+        self.test_index = images[N - self.test_count:]
+        self.test_label = labels[N - self.test_count:]
+        print('\nData loading done! %d pcs train data and %d pcs test data loaded.\n'%(N, self.test_count))
 
-        all_batch = int(len(src) / batch_size) - 1
-        this_batch = 0
+    def gen(self, batch_size=96, type='train'):
+        """读取图像数据并返回
+
+        Args:
+            batch_size(Int): 单个Batch包含样本数目
+            type(String): 'train' or 'test' 用于复用数据生成器
+
+        Return:
+            X(Array): 训练数据
+            y(List): 训练label
+        """
+        width, height = 240, 60
+        n_class, n_len = 10, 5 # 需要识别10个字符，5位数
+
+        images = self.train_index if type == 'train' else self.test_index
+        labels = self.train_label if type == 'train' else self.test_label
+        if type == 'test':
+            batch_size = self.test_count # 一次性返回所有测试数据
+
+        X = np.zeros((batch_size, height, width, 1), dtype=np.uint8)
+        y = [np.zeros((batch_size, n_class), dtype=np.uint8) for i in range(n_len)]
+
+        path_prefix = self.path_prefix # 数据文件前缀
+        all_batch = int(len(images) / batch_size) # 一个epoch包含的batch
+        this_batch = 0 # 当前batch
         while True:
             for i in range(batch_size):
                 index = this_batch * batch_size + i
@@ -65,6 +111,13 @@ class Captcha:
             # print(this_batch, all_batch)
             yield X, y
 
+class Captcha:
+    def __init__(self):
+        self.width, self.height, self.n_len, self.n_class = 240, 60, 5, 10
+        self.model = None
+        self.dataset = Dataset()
+        self.init_model()
+
     def init_model(self):
         width, height = self.width, self.height
         input_tensor = Input((height, width, 1))
@@ -73,7 +126,6 @@ class Captcha:
             x = Convolution2D(32*2**i, (2, 2), activation='relu')(x)
             x = Convolution2D(32*2**i, (2, 2), activation='relu')(x)
             x = MaxPooling2D((2, 2))(x)
-            print('adding %d'%i)
 
         x = Flatten()(x)
         x = Dropout(0.25)(x)
@@ -85,12 +137,14 @@ class Captcha:
 
     def train(self):
         self.checkpoint_path = './model.h5'
-        self.model.fit_generator(self.gen(128), steps_per_epoch=1000, epochs=10,
-            validation_data=self.gen(256), validation_steps=10)
+        custom_acc = Evaluator(self.dataset)
+        self.model.fit_generator(self.dataset.gen(128), steps_per_epoch=200, epochs=50,
+            validation_data=self.dataset.gen(256), validation_steps=10,
+            callbacks=[custom_acc])
         # 训练完成后，将模型保存起来
         self.model.save(self.checkpoint_path)
+        print('History acc:', custom_acc.accs)
         print('\nModel has been saved at %s'%self.checkpoint_path)
 
     def predict(self, X):
         self.model.predict(X)
-
--- keras-model/evaluate.py
+++ keras-model/evaluate.py
@@ -1,11 +1,19 @@
 from keras.models import load_model
 import os
 from captcha import Captcha
+import numpy as np
 
 os.environ['CUDA_VISIBLE_DEVICES'] = '1'
 
 model = load_model('model.h5')
-X_test, y_test = next(Captcha().gen(1000))
-y_pred = model.predict(X_test)
+dataset = Captcha()
 
-
+nums = 20
+acc = 0.0
+for i in range(nums):
+    X_test, y_test = next(dataset.gen(1000))
+    y_pred = model.predict(X_test)
+    pred = np.argmax(y_pred, axis=2).T
+    y_true = np.argmax(y_pred, axis=2).T
+    acc += np.mean(map(np.array_equal, pred, y_true))
+print(acc / nums)
