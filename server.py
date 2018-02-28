f381487fa51e25ff5e621a6da1c8488f57388c57
Yidadaa <1143922395.com>
2018-02-28 10:58:02 +0800

    feat: 为server添加异步支持以及错误处理

--- server.py
+++ server.py
@@ -4,9 +4,16 @@
 from flask import Flask, request, Response
 import numpy as np
 import scipy.misc as misc
+
 from model import Captcha
+
 import io
 import json
+
+from gevent import monkey
+from gevent.pywsgi import WSGIServer
+# support async
+monkey.patch_all()
 
 model = Captcha()
 model.load_checkpoint('crack_capcha0.994400002956.model-9960')
@@ -16,14 +23,18 @@ app = Flask(__name__)
 @app.route('/crack', methods=['GET', 'POST'])
 def crack_upload_img():
     if request.method == 'POST':
-        f = request.files['file']
-        img = f.read()
-        gif = misc.imresize(misc.imread(io.BytesIO(img)), [64, 256], interp='nearest')
-        img_array = np.array(gif)
-        text = model.predict(img_array)
-        res = {}
-        res['result'] = ''.join(map(str, text))
-        return Response(json.dumps(res), mimetype='application/json')
+        try:
+            f = request.files['file']
+            img = f.read()
+            gif = misc.imresize(misc.imread(io.BytesIO(img)), [64, 256], interp='nearest')
+            img_array = np.array(gif)
+            text = model.predict(img_array)
+            res = {}
+            res['result'] = ''.join(map(str, text))
+            return Response(json.dumps(res), mimetype='application/json')
+        except:
+            res = { "err": "Error! Please check your file." }
+            return Response(json.dumps(res), mimetype='application/json')
     return '''
         <!doctype html>
         <title>Upload your file</title>
@@ -35,4 +46,6 @@ def crack_upload_img():
     '''
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    # app.run(host='0.0.0.0', port=80)
+    http_server = WSGIServer(('', 80), app)
+    http_server.serve_forever()
